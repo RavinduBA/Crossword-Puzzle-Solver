7880576edd6cea999076a6fde53801cd1499d43e
RavindBA <e19004@eng.pdn.ac.lk>
2023-07-24 21:20:31 +0530

    add more
    adds
    
    Co-Authored-By: kalana kithmina <e19198@eng.pdn.ac.lk>

--- phase1.c
+++ phase1.c
@@ -8,3 +8,18 @@
 #define MX_cols 100
 #define coordinate 2
 #define MX_words 100
+
+char save_words[MX_words][MX_wlen];
+int rows,cols;
+int word_available[MX_words];
+char Grid[MX_rows][MX_cols];
+int w_count;
+int location[coordinate];
+int Check_valid_row(int index, int a, int b);
+int Check_valid_column(int index, int a, int b);
+int check_letter(char Char);
+int find_();
+int find2();
+void Grid_print();
+int Valid_grid();
+int clarify();
