e5733a84ea1604a82aa4915077e46c548923d507
RavindBA <e19004@eng.pdn.ac.lk>
2023-07-24 21:02:45 +0530

    Phase I - Static memory allocation started

---
+++ phase1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<ctype.h>
+#include<string.h>
+
+#define coordinate 2
+#define MX_rows 100
+#define MX_wlen 100
+#define MX_cols 100
+#define coordinate 2
+#define MX_words 100
